77cfd8770fbed9dc2ffcc266e392418d10feb088
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-18 16:11:34 +0000

    minor changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,14 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if ((array[j] > array[j + 1]) && (j + 1 != size))
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
