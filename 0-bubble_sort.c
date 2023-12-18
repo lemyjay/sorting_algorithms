b94270a4f5011ca2265ae045cb07690694646507
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-18 12:29:34 +0000

    the files tto task 0, but are not fully implemented

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - A function that sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ *
+ * @array: The array containing the elements to be sorted
+ * @size: The size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	something;
+}
